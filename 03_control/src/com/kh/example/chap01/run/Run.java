bf9ce70a459c0f89637b223d1ed18d1daefd9e4a
eunhye3333 <eunhye_3@naver.com>
2021-08-11 16:40:49 +0900

    학습 중

---
+++ 03_control/src/com/kh/example/chap01/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.example.chap01.run;
+
+import com.kh.example.chap01.condition.A_If;
+
+public class Run {
+	public static void main(String[] args) {
+		A_If ai = new A_If();
+//		ai.method1();
+//		ai.method2();
+		ai.method3();
+	}
+}
