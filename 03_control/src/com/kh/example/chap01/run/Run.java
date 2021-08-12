4c9dba59a16fbc351fde57dffd1b7871df78353b
eunhye3333 <eunhye_3@naver.com>
2021-08-12 18:47:56 +0900

    조건문 과제 완료

--- 03_control/src/com/kh/example/chap01/run/Run.java
+++ 03_control/src/com/kh/example/chap01/run/Run.java
@@ -1,12 +1,19 @@
 package com.kh.example.chap01.run;
 
 import com.kh.example.chap01.condition.A_If;
+import com.kh.example.chap01.condition.B_Switch;
 
 public class Run {
 	public static void main(String[] args) {
-		A_If ai = new A_If();
-//		ai.method1();
+		A_If ai = new A_If(); // 클래스가 달라서 알맹이를 만들어 줌
+//		ai.method1(); // A_if 안에 있는 메소드 출ㄺ
 //		ai.method2();
-		ai.method3();
+//		ai.method3();
+		
+		B_Switch bs = new B_Switch();
+//		bs.method1();
+//		bs.method2();
+		bs.method3();
+		
 	}
 }
